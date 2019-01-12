0214ed0f9dba3d87288af97745339a8532aebbcd
Dennis Fischer <dennis.fischer@live.com>
2019-01-12 15:21:38 +0100

    Added file to set perms for benchexec

---
+++ perms.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+chmod o+wt '/sys/fs/cgroup/freezer/'
+chmod o+wt '/sys/fs/cgroup/memory/user.slice/user-1000.slice/user@1000.service'
+chmod o+wt '/sys/fs/cgroup/cpu,cpuacct/user.slice'
+chmod o+wt '/sys/fs/cgroup/cpuset/'
+chmod o+wt '/sys/fs/cgroup/cpu,cpuacct/'
+swapoff -a
